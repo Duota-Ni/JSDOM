9cafb0ff88f78a44e6c67b705ef45d8a418aee0b
Duota <duota001@163.com>
2020-07-03 22:11:34 +0800

    Chaper 6

--- main/scripts/showPic.js
+++ main/scripts/showPic.js
@@ -1,12 +1,48 @@
+//第六章
 function showPic(whichpic) {
+    //"placeholder"是否存在
+    if (!document.getElementById("placeholder")) return false;
     //图片切换
     let source = whichpic.getAttribute('href');
     let placeholder = document.getElementById('placeholder');
+    //检查图片
+    if (placeholder.nodeName != "IMG") return false;
     placeholder.setAttribute("src", source);
-    //文本切换
+    //文本切换(可选)
+    if (document.getElementById("description")) {
+        let text = whichpic.getAttribute('title') ? whichpic.getAttribute('title') : "";
+        let description = document.getElementById('description');
+        if (description.firstChild.nodeType == 3) {
+            description.firstChild.nodeValue = text;
+        }
+    }
+    return true;
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    let gallery = document.getElementById("imagegallery");
+    let links = gallery.getElementsByTagName("a");
+    for (let i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            return !showPic(this);
+        }
+    }
+}
+//调用
+window.onload = prepareGallery();
+
+/*
+//第五章及之前
+function showPic(whichpic){
+    let source = whichpic.getAttribute('href');
+    let placeholder = document.getElementById('placeholder');
+    placeholder.setAttribute("src", source);
     let text = whichpic.getAttribute('title');
     let description = document.getElementById('description');
-    description.firstChild.nodeValue = text;
+    description.firstChild.nodeValue=text;
 }
 
 function popUp(winURL) {
@@ -26,6 +62,9 @@ window.onload = function () {
         }
     }
 }
+*/
+
+
 /*
 function countBodyChildern(){
     let body_element  = document.getElementsByTagName('body')[0];
