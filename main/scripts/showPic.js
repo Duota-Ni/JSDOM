f0d89b845aae497529367cfe2356a5fa0ea2ea3e
Duota <duota001@163.com>
2020-07-05 22:02:56 +0800

    Chapter7  image gallery all

--- main/scripts/showPic.js
+++ main/scripts/showPic.js
@@ -1,16 +1,62 @@
-//第七章动态创建标记
-function createLast(){
-    let placeholder = document.createElement('img');
-    placeholder.setAttribute('id','placeholder');
-    placeholder.setAttribute('src','images/kong.jpg');
-    placeholder.setAttribute('alt','my image gallery')
-    let description = document.createElement('p');
-    description.setAttribute('id','description');
-    let  destext = document.createTextNode("Choose an image.");
-    description.appendChild(destext);
-    document.getElementsByTagName('body')[0].appendChild(placeholder);
-    document.getElementsByTagName('body')[0].appendChild(description);
+//最终
+//共享onload,多个function
+function addLoadEvent(func) {
+    let oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+//在目标元素后面插入
+function insertAfter(newElement, targetElement) {
+    let parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    }else{
+        parent.insertBefore(newElement,targetElement.nextSibling);
+    }
+}
 
+//第七章动态创建标记
+function preparePlaceholder() {
+    if(!document.createElement) return false;
+    if(!document.createTextNode) return false;
+    if(!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    let placeholder = document.createElement('img');
+    placeholder.setAttribute('id', 'placeholder');
+    placeholder.setAttribute('src', 'images/kong.jpg');
+    placeholder.setAttribute('alt', 'my image gallery')
+    let description = document.createElement('p');
+    description.setAttribute('id', 'description');
+    let destext = document.createTextNode("Choose an image.");
+    description.appendChild(destext);
+    //new
+    let gallery = document.getElementById('imagegallery');
+    insertAfter(placeholder,gallery);
+    insertAfter(description,placeholder);
+    //document.getElementsByTagName('body')[0].appendChild(placeholder);
+    //document.getElementsByTagName('body')[0].appendChild(description);
+
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    let gallery = document.getElementById("imagegallery");
+    let links = gallery.getElementsByTagName("a");
+    for (let i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            return !showPic(this);
+        }
+        //键盘
+        links[i].onkeypress = links[i].onclick;
+    }
 }
 
 //第六章
@@ -24,6 +70,7 @@ function showPic(whichpic) {
     if (placeholder.nodeName != "IMG") return false;
     placeholder.setAttribute("src", source);
     //文本切换(可选)
+    if(!document.getElementById("description")) return false;
     if (document.getElementById("description")) {
         let text = whichpic.getAttribute('title') ? whichpic.getAttribute('title') : "";
         let description = document.getElementById('description');
@@ -34,20 +81,12 @@ function showPic(whichpic) {
     return true;
 }
 
-function prepareGallery() {
-    if (!document.getElementsByTagName) return false;
-    if (!document.getElementById) return false;
-    if (!document.getElementById("imagegallery")) return false;
-    let gallery = document.getElementById("imagegallery");
-    let links = gallery.getElementsByTagName("a");
-    for (let i = 0; i < links.length; i++) {
-        links[i].onclick = function () {
-            return !showPic(this);
-        }
-    }
-}
+
 //调用
-window.onload=function(){
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
+/*
+window.onload = function () {
     createLast();
     prepareGallery();
 }
