32a8d09af3b779e920d269282639099ad0348e43
Duota <duota001@163.com>
2020-07-19 16:42:00 +0800

    about and live and photos

--- project/scripts/global.js
+++ project/scripts/global.js
@@ -139,3 +139,177 @@ function prepareSlideshow(){
     }
 }
 addLoadEvent(prepareSlideshow);
+function showSection(id){
+    let sections = document.getElementsByTagName('section');
+    for(let i = 0 ; i<sections.length;i++){
+        if(sections[i].getAttribute('id') != id){
+            sections[i].style.display = 'none';
+        }else{
+            sections[i].style.display = 'block';;
+        }
+    }
+}
+function prepareInternalnav(){
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById) return false;
+    let articles = document.getElementsByTagName('article');
+    if(articles.length == 0) return false;
+    let navs = articles[0].getElementsByTagName('nav');
+    if(navs.length == 0) return false;
+    let nav = navs[0];
+    let links = nav.getElementsByTagName('a');
+    for(let i = 0 ;i<links.length;i++){
+        let sectionId = links[i].getAttribute('href').split('#')[1];
+        if(!document.getElementById(sectionId)) continue;
+        document.getElementById(sectionId).style.display = 'none';
+        links[i].destination = sectionId;
+        links[i].onclick = function(){
+            showSection(this.destination);
+            return false;
+        }
+    }
+}
+addLoadEvent(prepareInternalnav);
+
+function showPic(whichpic) {
+    //"placeholder"是否存在
+    if (!document.getElementById("placeholder")) return false;
+    //图片切换
+    let source = whichpic.getAttribute('href');
+    let placeholder = document.getElementById('placeholder');
+    //检查图片
+    if (placeholder.nodeName != "IMG") return false;
+    placeholder.setAttribute("src", source);
+    //文本切换(可选)
+    if(!document.getElementById("description")) return false;
+    if (document.getElementById("description")) {
+        let text = whichpic.getAttribute('title') ? whichpic.getAttribute('title') : "";
+        let description = document.getElementById('description');
+        if (description.firstChild.nodeType == 3) {
+            description.firstChild.nodeValue = text;
+        }
+    }
+    return true;
+}
+function preparePlaceholder() {
+    if(!document.createElement) return false;
+    if(!document.createTextNode) return false;
+    if(!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    let placeholder = document.createElement('img');
+    placeholder.setAttribute('id', 'placeholder');
+    placeholder.setAttribute('src', 'images/imagegallery/placeholder.gif');
+    placeholder.setAttribute('alt', 'my image gallery')
+    let description = document.createElement('p');
+    description.setAttribute('id', 'description');
+    let destext = document.createTextNode("Choose an image.");
+    description.appendChild(destext);
+    //new
+    let gallery = document.getElementById('imagegallery');
+    insertAfter(placeholder,gallery);
+    insertAfter(description,placeholder);
+    //document.getElementsByTagName('body')[0].appendChild(placeholder);
+    //document.getElementsByTagName('body')[0].appendChild(description);
+
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    let gallery = document.getElementById("imagegallery");
+    let links = gallery.getElementsByTagName("a");
+    for (let i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            return !showPic(this);
+        }
+        //键盘
+        links[i].onkeypress = links[i].onclick;
+    }
+}
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
+function stripeTables() {
+    if (!document.getElementsByTagName) return false;
+    let tables = document.getElementsByTagName("table");
+    let odd, rows;
+    for (let i = 0; i < tables.length; i++) {
+        odd = false;
+        rows = tables[i].getElementsByTagName("tr");
+        for (let j = 0; j < rows.length; j++) {
+            if (odd == true) {
+                //rows[j].style.backgroundColor = "#ffc";
+                addClass(rows[j],"odd");
+                odd = false;
+            } else {
+                odd = true;
+            }
+        }
+    }
+}
+addLoadEvent(stripeTables);
+function highlightRows() {
+    if (!document.getElementsByTagName) return false;
+    let rows = document.getElementsByTagName("tr");
+    for (let i = 0; i < rows.length; i++) {
+        rows[i].oldClassName = rows[i].className;
+        rows[i].onmouseover = function(){
+            addClass(this,'highlight');
+        }
+        rows[i].onmouseout = function(){
+            this.className = this.oldClassName;
+        }
+    }
+}
+addLoadEvent(highlightRows);
+function displayabbreviations() {
+    if (!document.getElementsByTagName || !document.createElement
+        || !document.createTextNode) return false;
+    //取得所有略缩词
+    let abbreviations = document.getElementsByTagName("abbr");
+    let abbrlen = abbreviations.length;
+    if (abbrlen < 1) return false;
+    let defs = new Array();
+    //遍历这些略缩词
+    for (let i=0; i < abbrlen; i++) {
+        let current_abbr = abbreviations[i];
+        //IE7的平稳退化
+        if(current_abbr.childNodes.length<1) continue;
+
+        let definition = current_abbr.getAttribute("title");
+        let key = current_abbr.lastChild.nodeValue;
+        defs[key] = definition;
+    }
+    //创建定义列表
+    let dlist = document.createElement("dl");
+    //遍历定义
+    for (key in defs) {
+        let definition = defs[key];
+        //创建定义标题
+        let dtitle = document.createElement("dt");
+        let dtitle_text = document.createTextNode(key);
+        dtitle.appendChild(dtitle_text);
+        //创建定义描述
+        let ddesc = document.createElement("dd");
+        let ddesc_text = document.createTextNode(definition);
+        ddesc.appendChild(ddesc_text);
+        //把他们添加到定义列表
+        dlist.appendChild(dtitle);
+        dlist.appendChild(ddesc);
+    }
+    //IE7的平稳退化
+    if(dlist.childNodes.length<1) return false;
+
+    //创建标题
+    let header = document.createElement("h2");
+    let header_text = document.createTextNode("注释：");
+    header.appendChild(header_text);
+    let articles = document.getElementsByTagName('article');
+    if(articles.length == 0) return false;
+    let container = articles[0];
+    //把标题添加到页面主体
+    container.appendChild(header);
+    //把定义列表添加到页面主体
+    container.appendChild(dlist);
+}
+addLoadEvent(displayabbreviations);
